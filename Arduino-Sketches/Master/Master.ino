cbace2ab458de499c16d85e794760a744722d029
Davide Becker <github@davide.me>
2018-06-30 13:58:58 +0200

    Improve comments in sketch files

--- Arduino-Sketches/Master/Master.ino
+++ Arduino-Sketches/Master/Master.ino
@@ -1,24 +1,26 @@
 // SimpleTx - the master or the transmitter
 
+// Include the required libraries
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include "HX711.h" //You must have this library in your arduino library folder
+#include "HX711.h" // You must have this library in your arduino library folder
+                   // https://github.com/bogde/HX711
 
 //////////////
 // Wireless //
 //////////////
 
+// Which pins to use & Setup
 #define CE_PIN   7
 #define CSN_PIN  8
+RF24 radio(CE_PIN, CSN_PIN);
 
 const byte slaveAddress[5] = {'R','x','A','A','A'};
 
-
-RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
-
 char txNum = '0';
 
+// Timing variables
 unsigned long currentMillis;
 unsigned long prevMillis;
 unsigned long txIntervalMillis = 100; // send once per second
@@ -27,9 +29,9 @@ unsigned long txIntervalMillis = 100; // send once per second
 // Scale //
 ///////////
 
+// Which pins to use & Setup
 #define DOUT 3
 #define CLK 2
-
 HX711 scale(DOUT, CLK);
 
 //Change this calibration factor as per your load cell once it is found you many need to vary it in thousands
@@ -40,71 +42,55 @@ float calibration_factor = 106600; //-106600 worked for my 40Kg max scale setup
 //////////////////////
 // https://funduino.de/nr-8-bewegungsmelder
 
+// Which pins to use
 int motionSensorPin = 5;
 int motionSensorState = 0;
 int motionDetectionCounter = 0;
 bool motionAlreadyDetected = false;
 
+//////////////////
+// Main sketch //
+/////////////////
 
-//////////
-// Data //
-//////////
 
-struct sensorData {
-  int visitors = 0;
-  float weight = 0;
-};
-
-int debugLED = 4;
-
-sensorData data;
-
-//////////////////////
-// Helper functions //
-//////////////////////
-
-unsigned concatNumbers(unsigned x, unsigned y) {
-  unsigned pow = 10;
-  while(y >= pow)
-    pow *= 10;
-  return x * pow + y;        
-}
-
+//==================== Setup
 
 void setup() {
+  // Start debug console
   Serial.begin(9600);
-  
   Serial.println("SimpleTx Starting");
   
+  // Setup pins on the arduino
   pinMode(motionSensorPin, INPUT);
   pinMode(debugLED, OUTPUT);
   
+  // Start up the wireless transmitter
   radio.begin();
   radio.setDataRate( RF24_250KBPS );
   radio.setRetries(3,5); // delay, count
   radio.openWritingPipe(slaveAddress);
   
+  // Initlaize the scale
   scale.set_scale(calibration_factor); //Calibration Factor obtained from first sketch
   scale.tare(); //Reset the scale to 0
 }
 
-//====================
+//==================== Loop
 
 void loop() {
-  currentMillis = millis();
-  if (currentMillis - prevMillis >= txIntervalMillis) {
-    //Serial.println(scale.get_units());
-    int weight = scale.get_units() * 1000 + 100;
-    //Serial.print("Scale weight: ");
-    //Serial.println(weight);
-    send(weight, 1);
-    prevMillis = millis();
+  currentMillis = millis(); // Get current time in milliseconds
+  if (currentMillis - prevMillis >= txIntervalMillis) { // If the specified interval is over
+    int weight = scale.get_units() * 1000 + 100; // Get the weight of the scale in grams + 100 
+                                                 // (So it doesn't underflow)
+    send(weight, 1); // Send the weight to the slave
+    prevMillis = millis(); // Update timing variable
   }
   
-  if(Serial.available()) {
-    char temp = Serial.read();
-    if(temp == 't' || temp == 'T')
-    scale.tare(); //Reset the scale to zero
+  if(Serial.available()) { // Listen for input
+    char temp = Serial.read(); // Get the input
+    if(temp == 't' || temp == 'T') { // If the input was "t"
+      scale.tare(); //Reset the scale to zero
+    }
   }
   
   motionSensorState = digitalRead(motionSensorPin);
@@ -123,29 +109,33 @@ void loop() {
   }
 }
 
-//====================
+//==================== Send
 
+// Helper function that sends a 32bit integer with a control bit
+// This allows the slave to match the data to either visitors or scale weight in this case
 void send(int dataToSend, int controlBit) {
   
-  unsigned char buff[5];
+  unsigned char buff[5]; // Create an empty buffer
   
-  buff[0] = controlBit;
+  buff[0] = controlBit; // Add the control bit to the buffer
+
+  // Split the data into four 8bit integer
   buff[1] = (dataToSend >> 24) & 0xFF;
   buff[2] = (dataToSend >> 16) & 0xFF;
   buff[3] = (dataToSend >>  8) & 0xFF;
   buff[4] = (dataToSend >>  0) & 0xFF;
   
-  bool rslt;
-  rslt = radio.write( &buff, 5 );
-  // Always use sizeof() as it gives the size as the number of bytes.
-  // For example if dataToSend was an int sizeof() would correctly return 2
-  
+  bool rslt = radio.write( &buff, 5 ); // Send the data and save the response
+
+  // Print the sent data to the serial console, formatted as a JSON object
+  // This allows node.js to grab the data
   Serial.print("{\"sent\":");
   Serial.print(controlBit);
   Serial.print(",\"data\":");
   Serial.print(dataToSend);
   Serial.print(",\"response\":");
   
+  // Check whether or not the slave sent a response back
   if (rslt) {
     Serial.print("true");
   }
--- Arduino-Sketches/Slave/Slave.ino
+++ Arduino-Sketches/Slave/Slave.ino
@@ -4,25 +4,40 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+//////////////
+// Wireless //
+//////////////
+
+// Which pins to use & Setup
 #define CE_PIN   7
 #define CSN_PIN  8
+RF24 radio(CE_PIN, CSN_PIN);
 
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+char dataReceived[5]; // this must match the length of dataToSend in the TX
+bool newData = false; // Allows to ignore duplicate data
+
+/////////////////////
+// Ambient Display //
+/////////////////////
+
+// Which pins to use
 #define notifyPin 5
 #define redPin    4
 #define greenPin  3
 #define bluePin   2
 
-const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
-
-RF24 radio(CE_PIN, CSN_PIN);
-
-char dataReceived[5]; // this must match dataToSend in the TX
-bool newData = false;
-
 //===========
 
-void HSVtoRGB(int hue, int sat, int val, int colors[3]) 
-  {
+//////////////////////
+// Helper functions //
+//////////////////////
+
+// Converts HSV to RGB
+// HSV allows for a better transition between colors like red and green
+// We need the RGB values for the LED though
+// http://forum.arduino.cc/index.php?topic=264605.0
+void HSVtoRGB(int hue, int sat, int val, int colors[3]) {
   // hue: 0-359, sat: 0-255, val (lightness): 0-255
   int r, g, b, base;
   if (sat == 0) 
@@ -71,72 +86,84 @@ void HSVtoRGB(int hue, int sat, int val, int colors[3])
       colors[1] = g;
       colors[2] = b;
     }
-  }
+}
 
-//===========
+// Shortcut to set the colors of the RGB LED
+void setRgbColor(unsigned int red, unsigned int green, unsigned int blue) { 
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+}
+
+/////////////////
+// Main sketch //
+/////////////////
+
+//==================== Setup
 
 void setup() {
-  
+  // Start debug console
   Serial.begin(9600);
-  
   Serial.println("SimpleRx Starting");
+  
+  // Start up the wireless transmitter
   radio.begin();
   radio.setDataRate( RF24_250KBPS );
   radio.openReadingPipe(1, thisSlaveAddress);
   radio.startListening();
 }
 
-//=============
+//==================== Loop
 
 void loop() {
-  getData();
-  showData();
+  getData();  // Watch for data
+  showData(); // Handle the data
 }
 
-//=============
+//==================== Watches for new data
 
 void getData() {
-  if ( radio.available() ) {
-    radio.read( &dataReceived, sizeof(dataReceived) );
-    newData = true;
+  if ( radio.available() ) { // If the radio is ready
+    radio.read( &dataReceived, 5 ); // Read data
+    newData = true; // We got new data available
+    // Little workaround, because the straight-forward approach was
+    // super unreliable. Two functions somehow work better
   }
 }
 
-//=============
+//==================== Data handler
 
 void showData() {
-  if (newData == true) {
-    analogWrite(notifyPin, 0);
-    Serial.print("{\"type\":");
+  if (newData == true) { // If we have new data available
+    digitalWrite(notifyPin, 0); // Turn off the notification LED
     
-    Serial.print((uint8_t)dataReceived[0]);
+    // Turn the recieved 4x8bit integers back to a 32bit integer
     uint32_t num = (uint8_t)dataReceived[1] << 24 |
                    (uint8_t)dataReceived[2] << 16 |
                    (uint8_t)dataReceived[3] << 8  |
                    (uint8_t)dataReceived[4];
+
+    if((uint8_t)dataReceived[0] == 1) { // If the control pin is 1 (Scale weight)
+      int h = (float)(num / 20000.0) * 125; // Get the percentage of the total weight
+                                            // (20 KG) and calculate a hue from it
+      int col[3]; // Create an array where we'll store the RGB values from the HSVtoRGB function
+      HSVtoRGB(h, 255, 255, col); // Convert HSL to RGB
+      setRgbColor(col[0], col[1], col[2]); // Change the color of the ambient display based on the scale weight
+    }
+
+    if((uint8_t)dataReceived[0] == 2) { // If the control pin is 2 (Visitor)
+      digitalWrite(notifyPin, 1); // Make the notification LED light up
+      delay(1000); // Wait a second
+    }
+
+    // Print the recieved data to the serial console, formatted as a JSON object
+    // This allows node.js to grab the data
+    Serial.print("{\"type\":");
+    Serial.print((uint8_t)dataReceived[0]);
     Serial.print(",\"value\":");
     Serial.print(num);
-
-    if((uint8_t)dataReceived[0] == 1) {
-      int h = (float)(num / 20000.0) * 125;
-      int col[3];
-      HSVtoRGB(h, 255, 255, col);
-      setRgbColor(col[0], col[1], col[2]);
-    }
-
-    if((uint8_t)dataReceived[0] == 2) {
-      digitalWrite(notifyPin, 1);
-      delay(1000);
-    }
-
     Serial.println("}");
     
-    newData = false;
+    newData = false; // Set newData back to false
   }
 }
-
-void setRgbColor(unsigned int red, unsigned int green, unsigned int blue) { 
-  analogWrite(redPin, red);
-  analogWrite(greenPin, green);
-  analogWrite(bluePin, blue);
- }

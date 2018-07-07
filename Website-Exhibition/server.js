215b765fdfd222d3968911644c973b42b47731ef
Davide Becker <hi@davide.me>
2018-07-07 12:17:11 +0200

    Add reconnect functionality

--- Website-Exhibition/server.js
+++ Website-Exhibition/server.js
@@ -1,4 +1,5 @@
 var SerialPort = require("serialport");
+const Readline = require('@serialport/parser-readline')
 
 var express = require('express');
 
@@ -8,29 +9,49 @@ var io = require('socket.io')(server);
 
 var serialportName = '/dev/cu.usbmodem1411'
 // var serialportName = 'COM6'
-
-const port = new SerialPort(serialportName, {
-    // baudRate: 38400
-    // baudRate: 115200
-    baudRate: 9600
-});
-if(port) {
-    const parser = new SerialPort.parsers.Readline();
-    port.pipe(parser);
-    parser.on('data', function(data) {
-        var parsedData
-
-        try {
-            parsedData = JSON.parse(data.toString())
-            console.log(parsedData)
-        } catch (e) {
-            console.error('!', data);
-            return false;
-        }
-
-        return io.emit('sensorData', parsedData)
-    });
+function reconnectArduino() {
+    console.log('INITIATING RECONNECT');
+    setTimeout(function () {
+        console.log('RECONNECTING TO ARDUINO');
+        connectToArduino();
+    }, 2000);
 }
+
+function connectToArduino() {
+    const port = new SerialPort(serialportName, {
+        // baudRate: 38400
+        // baudRate: 115200
+        baudRate: 9600
+    });
+    const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+
+    if (parser) {
+        parser.on('data', function (data) {
+            var parsedData
+            try {
+                parsedData = JSON.parse(data.toString())
+                console.log(parsedData)
+            } catch (e) {
+                console.error('!', data);
+                return false;
+            }
+
+            return io.emit('sensorData', parsedData)
+        });
+
+        port.on('close', function () {
+            console.log('ARDUINO PORT CLOSED');
+            reconnectArduino();
+        });
+
+        port.on('error', function (err) {
+            console.error("! ERROR", err);
+            reconnectArduino();
+        });
+    }
+}
+
+connectToArduino()
 
 io.on('connection', (socket) => {
     console.log('user connected')
@@ -38,7 +59,7 @@ io.on('connection', (socket) => {
 
 app.use('/assets', express.static(__dirname + '/assets'));
 
-app.get('/', function(req, res,next) {
+app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
 });
 
